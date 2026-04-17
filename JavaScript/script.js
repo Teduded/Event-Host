2c0f2802832462280e670fcfde0913bd88081a2d
Ted Lyder <ted.lyder@skola.taby.se>
2026-04-17 08:34:10 +0200

    vecka 16

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,26 @@
 const homeImage = document.getElementById("home-image")
-const prevButton = document.getElementById("previous-button")
-const nextButton = document.getElementById("next-button")
+const prevButton = document.getElementById("previous-picture")
+const nextButton = document.getElementById("next-picture")
+const getStartedButton = document.getElementById("getstarted")
 
-prevButton.onclick = function() {
-    
+const images = [
+    "../images/home-picture1.jpg",
+    "../images/home-picture2.jpg",
+    "../images/home-picture3.jpg",
+]
+
+let currentIndex = 0
+
+nextButton.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % images.length
+    homeImage.src = images[currentIndex]
+})
+
+prevButton.addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length
+    homeImage.src = images[currentIndex]
+})
+
+getStartedButton.onclick = function () {
+    window.location.href = "getstarted.html"
 }
